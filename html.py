22c9e2a6e0ebdc3560a9b403174e591bfe30d1b9
Patrick <pat.trainer@gmail.com>
2023-05-12 00:42:46 -0500

    feat: use jinja template

--- html.py
+++ html.py
@@ -1,17 +1,35 @@
+import hashlib
 import os
 import requests
+from jinja2 import Template
 
 
-def html_header(title):
-    return f'''
+def get_tempalate():
+    template = Template(
+        """
     <!DOCTYPE html>
     <html>
         <head>
-        <title>{title}</title>
-        {stylesheet()}
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width">
+            <title>{{ title }}</title>
+            
+            {{ stylesheet }}
         </head>
-    <body>
-    '''
+        <body>
+            <header>
+                <nav>
+                    <a href="patricktrainer.github.io/static-site">Home</a> /
+                    <a href="https://github.com/patricktrainer">GitHub</a> / 
+                </nav>
+            </header>
+            {{ content }}
+        </body>
+    </html>
+    """
+    )
+
+    return template
 
 
 def html_footer():
@@ -20,28 +38,33 @@ def html_footer():
     </html>
     """
 
+def html_elements(tag, elements):
+    return "".join([f"<{tag}>{el}</{tag}>" for el in elements])
+
 
 def html_table(cols, data):
     html = f"""
     <table>
     <tr>
-    {"".join([f"<th>{col}</th>" for col in cols])}
+    {html_elements('th', cols)}
     </tr>
     """
     for row in data:
         html += f"""
         <tr>
-        {"".join([f"<td>{col}</td>" for col in row])}
+        {html_elements('td', row)}
         </tr>
         """
-    html += """
-    </table>
-    """
+    html += "</table>"
     return html
 
 
 def html_page(title, content):
-    return html_header(title) + content + html_footer()
+    """Return a string containing the full HTML page."""
+    template = get_tempalate()
+    html = template.render(title=title, stylesheet=stylesheet(), content=content)
+
+    return html
 
 
 def html_list(items):
@@ -64,14 +87,10 @@ def html_link(url, text):
 
 def stylesheet():
     return """
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.min.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/open-fonts@1.1.1/fonts/inter.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@exampledev/new.css@1.1.2/new.min.css">
     <link rel="stylesheet" href="https://unpkg.com/github-syntax-dark@latest/lib/github-dark.css"/>
     """
-
-
-def html_style_css(style):
-    return f"""<style type="text/css">{style}</style>"""
-
 
 def get_posts(dir):
     posts = []
@@ -83,10 +102,23 @@ def get_posts(dir):
 
 def get_post_title(file):
     # strip .md from file name
-    return os.path.basename(file).replace(".md", "")
+    # use os.path.splitext to handle file extensions
+    return os.path.splitext(os.path.basename(file))[0]
 
 
 def github_md_to_html(body):
+    # create a unique hash for the body content
+    body_hash = hashlib.sha256(body.encode()).hexdigest()
+
+    # use the hash as a filename
+    cache_file = f"./cache/{body_hash}.html"
+
+    # check if the file exists
+    if os.path.exists(cache_file):
+        with open(cache_file, "r") as f:
+            return f.read()
+
+    # if it doesn't, make a request to the API
     response = requests.post(
         "https://api.github.com/markdown",
         json={"text": body},
@@ -96,7 +128,7 @@ def github_md_to_html(body):
         return response.text
     else:
         raise Exception(f"Error: {response.status_code} {response.text}")
-    
+
 
 def md_to_html(file):
     with open(file, "r") as f:
@@ -104,22 +136,29 @@ def md_to_html(file):
     return github_md_to_html(md)
 
 
-posts = get_posts("./posts")
+def main():
+    posts = get_posts("./posts")
 
-# write new html files for each post in blog directory
-for post in posts:
-    title = get_post_title(post)
-    content = md_to_html(post)
-    html = html_page(title, content)
-    with open(f"./blog/{title}.html", "w") as f:
+    # write new html files for each post in blog directory
+    for post in posts:
+        title = get_post_title(post)
+        content = md_to_html(post)
+        html = html_page(title, content)
+        with open(f"./blog/{title}.html", "w") as f:
+            f.write(html)
+
+    # strip .md from post names and create links
+    post_links = [
+        html_link(f"{post.replace('.md', '.html')}", get_post_title(post))
+        for post in posts
+    ]
+    blog_path = [b.replace("posts", "blog") for b in post_links]
+    content = html_list(blog_path)
+    html = html_page("Blog", content)
+    with open("./index.html", "w") as f:
         f.write(html)
 
-# strip .md from post names and create links
-post_links = [
-    html_link(f"{post.replace('.md', '.html')}", get_post_title(post)) for post in posts
-]
-blog_path = [b.replace("posts", "blog") for b in post_links]
-content = html_list(blog_path)
-html = html_page("Blog", content)
-with open("./index.html", "w") as f:
-    f.write(html)
+
+if __name__ == "__main__":
+
+    main()
